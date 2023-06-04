90b6cd6c595b4603d0d814d3b1a9c7da8a62d08a
alkher27 <alikheraj@gmail.com>
2023-06-04 20:48:07 +0000

    user auth complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #authenticate the user
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/places"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/places"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
-  
